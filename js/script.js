4128868164140b027281a638beb6113c5eb0af04
EderAmorimIFMS <eder.amorim@estudante.ifms.edu.br>
2023-09-02 21:33:25 -0400

    commit: alteração, front não esta 100% e javaScript está complicado

--- js/script.js
+++ js/script.js
@@ -1,91 +1,101 @@
-let l1c1, l1c2, l1c3, l2c1, l2c2, l2c3, l3c1, l3c2, l3c3;
-l1c1 = document.getElementById("C1");
-l1c2 = document.getElementById("C2");
-l1c3 = document.getElementById("C3");
-l2c1 = document.getElementById("C4");
-l2c2 = document.getElementById("C5");
-l2c3 = document.getElementById("C6");
-l3c1 = document.getElementById("C7");
-l3c2 = document.getElementById("C8");
-l3c3 = document.getElementById("C9");
+let c1, c2, c3, c4, c5, c6, c7, c8, c9;
+c1 = document.getElementById("C1");
+c2= document.getElementById("C2");
+c3 = document.getElementById("C3");
+c4 = document.getElementById("C4");
+c5 = document.getElementById("C5");
+c6 = document.getElementById("C6");
+c7 = document.getElementById("C7");
+c8 = document.getElementById("C8");
+c9 = document.getElementById("C9");
 
-l1c1.addEventListener('click', () => {})
-l1c2.addEventListener('click', () => {})
-l1c3.addEventListener('click', () => {})
-l2c1.addEventListener('click', () => {})
-l2c2.addEventListener('click', () => {})
-l2c3.addEventListener('click', () => {})
-l3c1.addEventListener('click', () => {})
-l3c2.addEventListener('click', () => {})
-l3c3.addEventListener('click', () => {})
+const possibilidadesVitoria = [
+   ["c1", "c2", "c3"],
+   ["c4", "c5", "c6"],
+   ["c7", "c8", "c9"],
+   ["c1", "c4", "c7"];
+   ["c2", "c5", "c8"];
+   ["c3", "c6", "c9"]; 
+   ["c1", "c5", "c9"];
+   ["c3", "c5", "c7"];
+]
 
-let playerX, playerO 
+let playerX, playerO;
+let qtd_jogadas = 0;
+playerO = "images/playerO.png";
+playerX ="images/playerO.png";
+let jogador = playerX;
 
-function vitoria(){
-    if(l1c1 == true &&  l1c2 == true && l1c3 == true){
-        /* 
-           c1 c2 c3
-        l1 y  y  y
-        l2
-        l3
-        */
+c1.addEventListener('click', () => {
+  qtd_jogadas++;
+  jogador = jogador === playerX ? playerO : playerX;
+  c1.style.backgroundImage = `url(${jogador})`;
+  vitoria()
+})
 
+c2.addEventListener('click', () => {
+  qtd_jogadas++;
+  jogador = jogador === playerX ? playerO : playerX;
+  c2.style.backgroundImage = `url(${jogador})`;
+  vitoria()
+})
 
-    } else if(l2c1 == true &&  l2c2 == true && l2c3 == true){
-          /* 
-           c1 c2 c3
-        l1 
-        l2 y  y  y
-        l3
-        */
+c3.addEventListener('click', () => {
+  qtd_jogadas++;
+  jogador = jogador === playerX ? playerO : playerX;
+  c3.style.backgroundImage = `url(${jogador})`;
+  vitoria()
+})
 
-    } else if(l3c1 == true &&  l3c2 == true && l3c3 == true){
-          /* 
-           c1 c2 c3
-        l1 
-        l2
-        l3 y  y  y
-        */
-        
-    } else if(l1c1 == true &&  l2c1 == true && l3c1 == true){
-          /* 
-           c1 c2 c3
-        l1 y
-        l2 y
-        l3 y
-        */ 
-        
-    } else if(l1c2 == true &&  l2c2 == true && l3c2 == true){
-          /* 
-           c1 c2 c3
-        l1    y
-        l2    y
-        l3    y
-        */
-        
-    } else if(l1c3 == true &&  l2c3 == true && l3c3 == true){
-          /* 
-           c1 c2 c3
-        l1        y
-        l2        y
-        l3        y
-        */
-        
-    } else if(l1c1 == true &&  l2c2 == true && l3c3 == true){
-          /* 
-           c1 c2 c3
-        l1 y
-        l2    y
-        l3       y
-        */
-        
-    }  else if(l1c3 == true &&  l2c2 == true && l3c1 == true){
-        /* 
-         c1 c2 c3
-      l1        y
-      l2    y
-      l3 y
-      */
-      
+c4.addEventListener('click', () =>{
+  qtd_jogadas++;
+  jogador = jogador === playerX ? playerO : playerX;
+  c4.style.backgroundImage = `url(${jogador})`;
+  vitoria()
+})
+
+c5.addEventListener('click', () => {
+  qtd_jogadas++;
+  jogador = jogador === playerX ? playerO : playerX;
+  c5.style.backgroundImage = `url(${jogador})`;
+  vitoria()
+})
+
+c6.addEventListener('click', () => {
+  qtd_jogadas++;
+  jogador = jogador === playerX ? playerO : playerX;
+  c6.style.backgroundImage = `url(${jogador})`;
+  vitoria()
+})
+
+c7.addEventListener('click', () => {
+  qtd_jogadas++;
+  jogador = jogador === playerX ? playerO : playerX;
+  c7.style.backgroundImage = `url(${jogador})`;
+  vitoria()
+})
+
+c8.addEventListener('click', () => {
+  qtd_jogadas++;
+  jogador = jogador === playerX ? playerO : playerX;
+  c8.style.backgroundImage = `url(${jogador})`;
+  vitoria()
+})
+
+c9.addEventListener('click', () => {
+  qtd_jogadas++;
+  jogador = jogador === playerX ? playerO : playerX;
+  c9.style.backgroundImage = `url(${jogador})`;
+  vitoria()
+})
+
+function vitoria() {chanches possi
+    const [a, b, c] = combination; 
+    if (cells[a] === currentPlayer && cells[b] === currentPlayer && cells[c] === currentPlayer) { 
+      winner = currentPlayer; status.textContent = `O jogador ${winner} venceu!`; return; } }
+  
+  
+  if(qtd_jogadas == 9){
+    alert("O jogo deu empate!")
   }
 }
